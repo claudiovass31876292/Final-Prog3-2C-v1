6bb2b8ed4d98e19881c1b4cb1cba84432c175ffa
JeremiasGaleano <63808502+JeremiasGaleano@users.noreply.github.com>
2020-11-16 22:22:26 -0300

    prueba 2
    prueba 2

---
+++ TP_FINAL_PROG3/Entidades/Categoria.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Categoria
+    {
+        private int id;
+        private string nombre; 
+
+        public Categoria()
+        {
+
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+    }
+
+}
